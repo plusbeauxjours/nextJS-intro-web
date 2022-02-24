1976ebb3714c3d3d3f2b9502ae30e38d1ca31b6f
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-24 22:18:31 +0700

    set: react-query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,32 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+
 import styled from "styled-components";
 import Seo from "../components/Seo";
+import { getMovies, IGetMoviesResult } from "./api/api";
 
 const Container = styled.div`
   color: tomato; ;
 `;
 
+const API_KEY = "01da2a336a604956c260900d1835847f";
+
 const Home: NextPage = () => {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+
   return (
     <>
       <Seo title="Home" />
-      <Container>Home</Container>
+      {isLoading && <h4>Loading...</h4>}
+      {data?.results?.map((movie) => (
+        <Container key={movie.id}>
+          <h4>{movie.title}</h4>
+        </Container>
+      ))}
     </>
   );
 };
